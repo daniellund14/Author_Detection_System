6fb67f19b2793d2649aa409051175efaae8f7433
Daniel Lund <daniel.lund14@gmail.com>
2017-02-27 22:50:02 -0700

    Added Frequency Mapper

---
+++ PA2/src/TermFrequencyMapper.java
@@ -0,0 +1,22 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Created by DanielLund on 2/27/17.
+ */
+public class TermFrequencyMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    public void map(LongWritable key, Text value, Mapper.Context context) throws
+            IOException, InterruptedException{
+
+        String[] termFrequency = value.toString().split("\\s+");
+        String author = termFrequency[0];
+        String term = termFrequency[1];
+        String frequency = termFrequency[2];
+        context.write(new Text(frequency + "\t" + term + "\t" + author), new Text(""));
+
+    }
+}
